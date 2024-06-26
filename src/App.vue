06d18cc0753d453383c9f0a574932a91307f78a7
yngmjs <mario.mejias.sb@gmail.com>
2024-06-26 17:04:33 -0400

    .

--- src/App.vue
+++ src/App.vue
@@ -42,8 +42,8 @@ import HelloWorld from './components/HelloWorld.vue'
           </li>
         </ul>
         <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <input class="form-control me-2" type="search" placeholder="Buscar Pelicula" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Buscar</button>
         </form>
       </div>
     </div>
