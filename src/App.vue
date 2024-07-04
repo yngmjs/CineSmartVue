589880899fb37ac74435111b6c665b994b30d83b
yngmjs <mario.mejias.sb@gmail.com>
2024-07-04 01:38:35 -0400

    .

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
-            <RouterLink class="nav-link active" aria-current="page" to="/">Peliculas</RouterLink>
+            <RouterLink class="nav-link" aria-current="page" to="/">Peliculas</RouterLink>
           </li>
           <li class="nav-item">
             <RouterLink class="nav-link" to="/">Sucursales</RouterLink>
