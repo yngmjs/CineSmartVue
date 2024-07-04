dc3dd89b2e86d632f6c2237ea286329ef5025b24
yngmjs <mario.mejias.sb@gmail.com>
2024-07-03 19:17:50 -0600

    .

--- src/components/Confiteria/ConfiteriaComponent.vue
+++ src/components/Confiteria/ConfiteriaComponent.vue
@@ -6,7 +6,35 @@
 <div class="container">
     <h1 class="text-white">Confiteria</h1>
     <hr>
-    
+    <div class="row">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="/src/assets/promosky.jpg" class="card-img-top" alt="...">
+                <!-- <div class="card-body">
+                    <h4 class="card-title">Promocion Rapido y Furioso</h4>
+                    <p class="card-text">Bowl Auto + Coca-Cola 500 ml</p>
+                </div> -->
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><h4 class="card-title">Promocion Rapido y Furioso</h4>
+                        <p class="card-text">Bowl Auto + Coca-Cola 500 ml</p>
+                        <hr>
+                        <div class="row">
+                            <div class="col-7">
+                                <h2 class="text-danger">$28.990</h2>
+                            </div>
+                            <div class="col-4">
+                                <button class="btn btn-primary">Comprar</button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <!-- <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div> -->
+            </div>
+        </div>
+    </div>
 </div>
 <br>
 </template>
